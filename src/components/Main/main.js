beaf11fac7bb7a04950fdad3c8e5931674835d00
Felipe Schneeberger <itsshompi@gmail.com>
2018-09-13 23:43:07 -0300

    Actividades Universitarias listas

--- src/components/Main/main.js
+++ src/components/Main/main.js
@@ -60,11 +60,11 @@ export default class Main extends Component {
             </ul>
             <ul className="list">
               <li>Aplicacion Web DentalApp (2016)</li>
-              <li></li>
+              <li>Aplicación que permite poder llevar un registro de los pacientes, ademas de poder </li>
             </ul>
             <ul className="list">
               <li>Aplicacion Desktop "Calculadora Colegio Bautista" (2016)</li>
-              <li></li>
+              <li>Aplicación que permite calcular el interes por mora o interes de un cheque a fecha en base a los parametros establecidos por el colegio.</li>
             </ul>
             <ul className="list">
               <li>Aplicacion Web CloudEducation (2014)</li>
@@ -72,6 +72,35 @@ export default class Main extends Component {
             </ul>
 
           </p>
+          <hr className="divisor"/>
+          <h3 className="sub-title"><span>Actividades Universitarias:</span> </h3>
+          <p className="copy">
+            <ul className="list">
+              <li>PRESIDENTE CENTRO DE ALUMNOS INGENIERIA CIVIL INFORMÁTICA</li>
+              <li>Universidad Catolica de Temuco 2018 - 2019</li>
+            </ul>
+            <ul className="list">
+              <li>INVESTIGADOR COLABORADOR EN PROYECTO FONDECYT 11170583 UCT</li>
+              <li>Universidad Catolica de Temuco - 07/03/2018 al 01/08/2018</li>
+            </ul>
+            <ul className="list">
+              <li>AYUDANTIA EN INTRODUCCIÓN A LOS SISTEMAS DE BASE DE DATOS. UCT</li>
+              <li>Universidad Catolica de Temuco - 07/03/2018 al 01/08/2018</li>
+            </ul>
+            <ul className="list">
+              <li>CLASES DE REFORZAMIENTO DE MATEMÁTICA PARA QUINTO BÁSICO.</li>
+              <li>Universidad Catolica de Temuco - 01/09/2017 al 04/11/2017</li>
+            </ul>
+            <ul className="list">
+              <li>CREACIÓN DE HERRAMIENTA PARA DISMINUIR LA SEGREGACIÓN SOCIOECONÓMICA.</li>
+              <li>Universidad Catolica de Temuco - 03/04/2017 al 08/11/2017</li>
+            </ul>
+            <ul className="list">
+              <li>AYUDANTIA TALLER DE INTEGRACIÓN I</li>
+              <li>Universidad Catolica de Temuco - 08/09/2016 al 12/12/2016</li>
+            </ul>
+          </p>
+
           <hr className="divisor"/>
           <h3 className="sub-title"><span>Premios:</span> </h3>
           <p className="copy">
