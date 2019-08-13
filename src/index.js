4160020a599c392946a5cd463d82144ce57a7f39
Felipe Shømpi Schneeberger <itsshompi@gmail.com>
2019-08-13 10:49:20 -0400

    Index

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom';
 //CSS
 import 'font-awesome/css/font-awesome.min.css';
 import 'animate.css/animate.min.css';
-import './index.css';
 
 
 import App from './components/App';
