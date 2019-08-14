0ab74beeffefab9251f14ea484f767b7f4008370
Felipe Schneeberger <itsshompi@gmail.com>
2019-08-14 15:13:01 -0400

    Se modifico el INdex

--- src/index.js
+++ src/index.js
@@ -5,10 +5,6 @@ import ReactDOM from 'react-dom';
 import 'font-awesome/css/font-awesome.min.css';
 import 'animate.css/animate.min.css';
 import './index.css';
-import './nada.css';
-
-asdasdsa nda
-
 
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
